a6670543adedda35c3772852976f8e50e49d99f5
FranckV <franck.vassallo@gmail.com>
2026-02-19 19:02:29 +0100

    chore: optimize background images (convert to webp)

--- js/data.js
+++ js/data.js
@@ -7,12 +7,12 @@
  */
 const DEFAULT_BGS = [
     // --- IMAGES PERMANENTES ---
-    "Fond_Equipe.png",
-    "Fond_Fafnir.png",
-    "Fond_Odyss%C3%A9e_des_Seigneurs_Dragons.png",
-    "Fond%20de%20feuille%20perso%20Iria.png",
-    "Fond%20de%20feuille%20perso%20Hedera.png",
-    "Fond%20de%20feuille%20perso%20Cephalus.png"
+    "Fond_Equipe.webp",
+    "Fond_Fafnir.webp",
+    "Fond_Odyss%C3%A9e_des_Seigneurs_Dragons.webp",
+    "Fond%20de%20feuille%20perso%20Iria.webp",
+    "Fond%20de%20feuille%20perso%20Hedera.webp",
+    "Fond%20de%20feuille%20perso%20Cephalus.webp"
 ];
 
 /**
